7cacdc011ea24953907bc87b377db43c1d942af4
andy19974a22 <andy19974a22@gmail.com>
2019-02-21 00:07:05 +0800

    feat(footer): footer 加上Github 連結

--- src/components-ui/footer.vue
+++ src/components-ui/footer.vue
@@ -13,12 +13,12 @@
                     <v-btn class="mx-3 white--text" icon><v-icon size="24px">icon-line</v-icon></v-btn></a>
 
             </v-flex>
-           <v-btn  v-for="link in links" v-if="link.to.search('http') === -1" :key="link.t" :to="langUrl() + link.to"  color="white" flat >
+           <v-btn v-for="link in links" v-if="link.to.search('http') === -1" :key="link.t" :to="langUrl() + link.to"  color="white" flat >
                 {{$t(link.t)}}
             </v-btn>
 
-            <a v-else class="unLine" href="https://drive.google.com/drive/folders/0B_CazUUBPCsWSDgxMU9NNEl6eDQ" target="_blank" >
-                <v-btn color="white" flat >
+            <a v-else class="unLine" :href="link.to" target="_blank" >
+                <v-btn color="white" flat style="text-transform: capitalize;">
                     {{$t(link.t)}}
                 </v-btn>
             </a>
@@ -26,7 +26,7 @@
                 powered by <strong><router-link :to="langUrl() + '/more/about'" class="unLine white--text">girls-frontline-toolset</router-link></strong>
             </v-flex>
             <v-flex xs12 py-2 text-xs-center white--text>
-                <img id="count_num" src="//counter1.fc2.com/counter_img.php?id=89451780">
+                <img alt="fc2 count" id="count_num" src="//counter1.fc2.com/counter_img.php?id=89451780">
             </v-flex>
         </v-layout>
     </v-footer>
@@ -39,7 +39,7 @@
         name: 'gl-ui-footer',
         data() {
             return {
-                links: [{"t":'footer.feedback',"to":"/more/feedback"}, {"t":'footer.link',"to":"/more/link"}, {"t":'footer.uploadImg',"to":"https//drive.google.com/drive/folders/0B_CazUUBPCsWSDgxMU9NNEl6eDQ"}, {"t":'footer.add-image',"to":"/image/add"}, {"t":'footer.about',"to":"/more/about"},{"t":'footer.update-log',"to":"/log/update"},{"t":"title.h1.more.sitemap","to":"/more/sitemap"},{"t":"footer.privacy-policy","to":"/more/privacy_policy"}]
+                links: [{"t":'footer.feedback',"to":"/more/feedback"}, {"t":'footer.link',"to":"/more/link"}, {"t":'footer.uploadImg',"to":"https//drive.google.com/drive/folders/0B_CazUUBPCsWSDgxMU9NNEl6eDQ"},{"t":'github',"to":"https://github.com/girls-frontline-toolset/website"}, {"t":'footer.add-image',"to":"/image/add"}, {"t":'footer.about',"to":"/more/about"},{"t":'footer.update-log',"to":"/log/update"},{"t":"title.h1.more.sitemap","to":"/more/sitemap"},{"t":"footer.privacy-policy","to":"/more/privacy_policy"}]
             }
         }, methods: {}, beforeCreate: function () {
         }, created: function () {
