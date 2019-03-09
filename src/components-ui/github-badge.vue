e2ca8a976a8504450f5b47b5ac1d3c9fc9adc517
andy19974a22 <andy19974a22@gmail.com>
2019-03-09 21:47:54 +0800

    feat(right content): Add github badge

---
+++ src/components-ui/github-badge.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container row pa-1 ref="r">
+      <v-layout justify-center>
+        <v-container row pa-0>
+          <v-flex xs12>
+            <a v-for="item in url" :key="item.alt" :href="item.href" style="margin-right: 5px">
+              <img :alt="item.alt" :src="item.src"/>
+            </a>
+          </v-flex>
+        </v-container>
+      </v-layout>
+    </v-container>
+</template>
+<script>
+    export default {
+        name: 'gl-ui-github-badge',
+        data() {
+            return {
+              url: [{alt:"GitHub stars",href:"https://github.com/girls-frontline-toolset/website",src:"https://img.shields.io/github/stars/girls-frontline-toolset/website.svg?color=%23ffb300&labelColor=%23000&style=flat-square"},
+                {alt:"GitHub issues",href:"https://github.com/girls-frontline-toolset/website/issues",src:"https://img.shields.io/github/issues/girls-frontline-toolset/website.svg?color=%23ffb300&labelColor=%23000&style=flat-square"},
+                {alt:"GitHub last commit",href:"https://github.com/girls-frontline-toolset/website/commits/master",src:"https://img.shields.io/github/last-commit/girls-frontline-toolset/website.svg?color=%23ffb300&labelColor=%23000&style=flat-square"}]
+            }
+        }
+    }
+</script>
--- src/components-ui/rightContent.vue
+++ src/components-ui/rightContent.vue
@@ -2,15 +2,17 @@
     <v-flex xs12 sm12 md3 mt-1 pa-1 v-resize="onResize">
         <gl-ui-event-schedule ref="hidden" :complete="complete"></gl-ui-event-schedule>
         <gl-ui-facebook ref="block" :style="PoClass"></gl-ui-facebook>
+        <gl-ui-github-badge :style="BadgeClass"></gl-ui-github-badge>
     </v-flex>
 </template>
 
 <script>
     import GlUiEventSchedule from "./eventSchedule";
     import GlUiFacebook from "./facebook";
+    import GlUiGithubBadge from "./github-badge";
 
     export default {
-        components: {GlUiFacebook, GlUiEventSchedule},
+        components: {GlUiGithubBadge, GlUiFacebook, GlUiEventSchedule},
         props: [''],
         commponents: {},
         name: 'gl-ui-right-content',
@@ -20,6 +22,7 @@
                 rightMargin: 0,
                 block: {width: 0, isPo: true},
                 PoClass: {},
+                BadgeClass:{},
                 lock: false,
                 isFirst: true
             }
@@ -33,17 +36,19 @@
                 } else {
                         this.hiddenHeight = this.$refs.hidden.$el.getBoundingClientRect().height;
                         this.block.width = this.$refs.block.$el.getBoundingClientRect().width;
-                    this.block.isPo = false;
+                        this.block.isPo = false;
                         this.checkFixed();
                 }
             },
             checkFixed() {
                 if (!this.block.isPo && (window.scrollY) > this.hiddenHeight) {
-                    this.PoClass = {'position': 'fixed', 'top': '53px', 'width': this.block.width + 'px'}
-                    this.block.isPo = true;
+                  this.PoClass = {'position': 'fixed', 'top': '53px', 'width': this.block.width + 'px'}
+                  this.BadgeClass = {'position': 'fixed', 'top': (this.$refs.block.$el.clientHeight + 50) + 'px', 'width': this.block.width + 'px'}
+                  this.block.isPo = true;
                 } else {
                     if ((window.scrollY) < this.hiddenHeight) {
                         this.PoClass = {};
+                        this.BadgeClass = {};
                         this.block.isPo = false;
                     }
                 }
